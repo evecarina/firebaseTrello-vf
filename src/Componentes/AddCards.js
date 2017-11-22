1dffc0524762e846033d4883800a459250568921
carina120794@gmail.com <you@example.com>
2017-11-22 09:45:18 -0500

    css

--- src/Componentes/AddCards.js
+++ src/Componentes/AddCards.js
@@ -9,16 +9,14 @@ class AddButton extends Component {
     }
   }
   render() {
-    const { list, board, card, boardId } = this.props;
+    const { list, board, card, IdBoard } = this.props;
     const funcion = list ? addList : board ? addBoard : addCard;
     const parametro = board ? '' : list ? list : card;
-    const change = () => {
+    const NewState = () => {
       this.setState({
         add: !this.state.add
       })
     }
-    const holder = list ? 'Add a New List...' : board ? 'Board Name' : '';
-    const message = list ? 'Add a New List...' : board ? 'Add new board...' : 'Add a New Card...';
     return (
       <div className={list ? 'list' : ''}>
         {
@@ -26,32 +24,32 @@ class AddButton extends Component {
             <div className={list || board ? 'box' : ''}>
               <form onSubmit={(e) => {
                 e.preventDefault();
-                card ? funcion(this.input.value, boardId, parametro) : funcion(this.input.value, parametro);
-                change();
+                card ? funcion(this.input.value, IdBoard, parametro) : funcion(this.input.value, parametro);
+                NewState();
               }}>
-                <FormGroup controlId="formControlsTextarea">
+                <FormGroup>
                   <FormControl
                     componentClass="textarea"
-                    placeholder={holder}
+                    placeholder="New List"
                     inputRef={ref => { this.input = ref; }}
                     onKeyDown={(e) => {
                       if (board && e.keyCode === 13) {
                         funcion(this.input.value, parametro);
-                        change();
+                        NewState();
                       }
                     }}
                     required />
                 </FormGroup>
                 <div>
                   <Button type='submit' >{list ? 'Save List' : board ? 'Create Board' : 'Add'}</Button>
-                  or <span className='cancel' onClick={change}>cancel</span>
+                  or <span className='cancel' onClick={NewState}>cancel</span>
                 </div>
               </form>
 
             </div>
             :
-            <div className={list || board ? 'box addNew' : 'addNew'} onClick={change}>
-              {message}
+            <div className={list || board ? 'box addNew' : 'addNew'} onClick={NewState}>
+              "New Boards"
             </div>
         }
       </div>
